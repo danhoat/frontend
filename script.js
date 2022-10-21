fbdc8fda62e6a088d9067bd5d64aaea6f0be246c
unknown <danhoat@gmail.com>
2022-10-22 05:00:27 +0700

    add files

---
+++ script.js
@@ -0,0 +1,13 @@
+(function($){
+	
+	$( document ).ready(function() {
+		
+
+		$("#showLogin").click(function(){
+			console.log('click');	
+			$("#modalLogin").fadeIn(1000);
+			$("body").toggleClass('modal-open')
+
+		});
+	})
+})(jQuery)
