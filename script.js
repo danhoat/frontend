c32998b1dc3a6f6c4609898ccb07ab8f7d903b08
unknown <danhoat@gmail.com>
2022-10-22 11:07:31 +0700

    add note

--- script.js
+++ script.js
@@ -2,12 +2,10 @@
 	
 	$( document ).ready(function() {
 		
-
 		$("#showLogin").click(function(){
 			console.log('click');	
 			$("#modalLogin").fadeIn(1000);
 			$("body").toggleClass('modal-open')
-
 		});
 	})
 })(jQuery)
